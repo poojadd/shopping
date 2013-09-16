69558197b0ae2bcfd2b8ed0ae2b6a20294f14e36
poojadd <pooja.dudhatra@gmail.com>
2013-09-16 12:42:06 +0530

    add initial iles and dir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopping::Application.config.secret_token = '464d340044c83b8189ba03c2476c21be2b26e43c4d1a72afa21671d6eee418a8904485d5b9a384b2ec842e40706d43c8a5b099b69de721b8ef7d7372227d776d'
